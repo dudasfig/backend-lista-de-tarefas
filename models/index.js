8b4d8f57fa64d240f513387058cf73394799845b
dudasfig <112442570+dudasfig@users.noreply.github.com>
2024-11-30 21:56:00 -0300

    update

--- models/index.js
+++ models/index.js
@@ -1,22 +1,11 @@
-const { Pool } = require("pg");
 require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // Lê a URL do banco de dados da variável de ambiente
+  connectionString: process.env.DATABASE_URL,
   ssl: process.env.DATABASE_URL.includes("localhost")
-    ? false // Sem SSL para localhost
-    : { rejectUnauthorized: false }, // Necessário para conexões seguras no Render
+    ? false
+    : { rejectUnauthorized: false },
 });
-
-(async () => {
-  try {
-    const client = await pool.connect();
-    console.log("Conexão com o banco de dados foi estabelecida com sucesso.");
-    client.release();
-  } catch (error) {
-    console.error("Erro ao conectar ao banco de dados:", error.message);
-    process.exit(1);
-  }
-})();
 
 module.exports = pool;
